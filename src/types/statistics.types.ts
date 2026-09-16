05511cc2bf3b4ba47f62029032774d3131ebc797
Mohammad Arkan <mdarkanurl@gmail.com>
2026-09-16 21:41:47 +0600

    Added types

---
+++ src/types/statistics.types.ts
@@ -0,0 +1,39 @@
+type TopSendersType = {
+    sender: string
+    email: string
+    count: number
+}
+
+type TopDomainsType = {
+    domain: string
+    count: number
+}
+
+type TopCategoriesType = {
+    categories: "Job Alter" | "Promoational"
+    count: number
+}
+
+type SourceBreakdownType = {
+    jobBoards: number
+    companies: number
+    newsletters: number
+    socialMedia: number
+    personal: number
+}
+
+type NewSendersType = {
+    sender: string,
+    firstSeen: Date
+}
+
+export type ResultType = {
+    uniqueSenders: number
+    uniqueDomains: number
+    topSenders: TopSendersType[]
+    topCategories: TopCategoriesType[]
+    topDomains: TopDomainsType[]
+    sourceBreakdown: SourceBreakdownType
+    newSenders: NewSendersType
+}
+
